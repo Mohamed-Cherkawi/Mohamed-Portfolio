b0a2aada013a0e2513d67fa3fdc323da3a1ae873
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-09-17 23:11:26 +0100

    finished the qualification and services sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,76 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 /*===== MENU SHOW =====*/
 
-    navToggle.addEventListener('click',() =>{
-        navMenu.classList.add('show-menu');
-    })
+navToggle.addEventListener("click", () => {
+  navMenu.classList.add("show-menu");
+});
 
 /*===== MENU HIDDEN =====*/
 
-navClose.addEventListener('click' , () =>{
-    navMenu.classList.remove('show-menu');
-    })
+navClose.addEventListener("click", () => {
+  navMenu.classList.remove("show-menu");
+});
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills__content'),
-// { element 1, element 2 , element 3 }
-skillsHeader = document.querySelectorAll('.skills__header');
+const skillsContent = document.getElementsByClassName("skills__content"),
+  // { element 1, element 2 , element 3 }
+  skillsHeader = document.querySelectorAll(".skills__header");
 // { element 1, element 2 , element 3 }
 function toggleSkills() {
-    let itemClass = this.parentNode.className ;
+  let itemClass = this.parentNode.className;
 
-    for (i = 0 ; i < skillsContent.length ; i++) {
-        skillsContent[i].className = 'skills__content skills__close';
-    }
-    if(itemClass === 'skills__content skills__close') {
-        this.parentNode.className = 'skills__content skills__open' ;
-    }
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener('click',toggleSkills);
-})
+  el.addEventListener("click", toggleSkills);
+});
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
 
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
 /*==================== SERVICES MODAL ====================*/
-
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-
 /*==================== TESTIMONIAL ====================*/
-
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
